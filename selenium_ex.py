18d0b82ba2b5a30a273b603fe4158a70d01d9ac0
dd-code-immersives <domilamad89@gmail.com>
2021-09-27 11:43:25 -0400

    inital commit

---
+++ selenium_ex.py
@@ -0,0 +1,27 @@
+# pylint: disable-all
+from selenium.webdriver import Chrome
+import pandas as pd
+
+webdriver = "/Users/andrewdoyle/Documents/selenium-example/chromedriver"
+
+driver = Chrome(webdriver)
+
+pages = 3
+
+total = []
+for page in range(1,pages):
+
+    url = "http://quotes.toscrape.com/js/page/" + str(page) + "/"
+
+    driver.get(url)
+
+    quotes = driver.find_elements_by_class_name("quote")
+    for quote in quotes:
+        quote_text = quote.find_element_by_class_name('text').text[1:-2]
+        author = quote.find_element_by_class_name('author').text
+        new = ((quote_text,author))
+        total.append(new)
+
+driver.close()
+df = pd.DataFrame(total,columns=['quote','author'])
+df.to_csv('quoted.csv')
